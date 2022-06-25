df851d7d17779e0386df8f74f4979cda5a440178
Calvin Cao <jtcao430@gmail.com>
2022-06-25 10:23:05 -0700

    update server

--- server/server.js
+++ server/server.js
@@ -3,19 +3,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 4000;
-const colorController = require('./controllers/colorController');
-const MONGO_URI = process.env.MONGO_URI;
+// const colorController = require('./controllers/colorController');
+// const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => {
-  console.log("DB connected");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", () => {
+//   console.log("DB connected");
+// });
 
 app.use(express.json());
 
@@ -23,17 +23,17 @@ app.get('/', (req, res) => {
   res.send('hello')
 });
 
-app.post('/colors', colorController.post, async (req, res) => {
-  return res.status(200).json(res.locals.doc);
-});
+// app.post('/colors', colorController.post, async (req, res) => {
+//   return res.status(200).json(res.locals.doc);
+// });
 
-app.get('/colors', colorController.get, (req, res) => {
-  return res.status(200).json(res.locals.colorsList);
-});
+// app.get('/colors', colorController.get, (req, res) => {
+//   return res.status(200).json(res.locals.colorsList);
+// });
 
-app.get('/random', colorController.getRandom, (req, res) => {
-  return res.status(200).json(res.locals.colorsList);
-});
+// app.get('/random', colorController.getRandom, (req, res) => {
+//   return res.status(200).json(res.locals.colorsList);
+// });
 
 // local error handler
 app.use((req, res) => {
